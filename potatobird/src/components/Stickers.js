469a7915cd86a0a8a764a1bd37d741628ce5d751
katieolson84 <katieolson84@gmail.com>
2022-03-08 16:56:13 -0700

    Sticker Data added

--- potatobird/src/components/Stickers.js
+++ potatobird/src/components/Stickers.js
@@ -1,9 +1,49 @@
-export default [
-    {
-        id: 1,
-        title: 'Mr. Lion',
-        price: 3.50
-        image: 
+import lion from '../images/lion.png'
+import snail from '../images/snail.png'
+import rhino from '../images/rhino.png'
+import bear from '../images/bear.png'
+import flamingo from '../images/flamingo.png'
+import pbird from '../images/pbird.png'
 
-    }
-]
+function stickers() {
+    return [
+        {
+            id: 1,
+            title: 'Mushroom Snail',
+            price: 3.50,
+            image: {snail}
+        },
+        {
+            id: 2,
+            title: 'Mossy Lion',
+            price: 3.50,
+            image: {lion}
+        },
+        {
+            id: 3,
+            title: 'Rocky Rhino',
+            price: 3.50,
+            image: {rhino}
+        },
+        {
+            id: 4,
+            title: 'Grassy Bear',
+            price: 3.50,
+            image: {bear}
+        },
+        {
+            id: 5,
+            title: 'Fancy Flamingo',
+            price: 3.50,
+            image: {flamingo}
+        },
+        {
+            id: 6,
+            title: 'Potato Bird',
+            price: 3.50,
+            image: {pbird}
+        },
+    ]
+}
+ 
+export default stickers;
